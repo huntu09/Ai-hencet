c337de333ba25570019d2ffa652cf1d7abbcd33f
huntu09 <memeksadas@gmail.com>
2025-06-12 01:29:00 +0700

    Create card.tsx

---
+++ components/ui/card.tsx
@@ -0,0 +1,6 @@
+export function Card({ children, className = "" }: any) {
+  return <div className={`border rounded bg-white ${className}`}>{children}</div>;
+}
+export function CardContent({ children, className = "" }: any) {
+  return <div className={className}>{children}</div>;
+}
