f8a06c43a30d1e736f300ae8a31823c893f80ad3
huntu09 <memeksadas@gmail.com>
2025-06-12 01:26:50 +0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import JSXEditor from "@/components/JSXEditor";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">JSX/TSX Live Editor</h1>
+      <JSXEditor />
+    </div>
+  );
+}
